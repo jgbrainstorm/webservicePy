8e4d1fd75a038b3655d77e56359511c08a2836b2
Jiangang Hao <jhao@ets.org>
2016-02-06 01:21:21 -0500

    a full web form + flask processing example is working

--- hello.py
+++ hello.py
@@ -1,18 +1,23 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 import json
 import pylab as pl
 import numpy as np
+from pattern.en import sentiment
 app = Flask(__name__)
 
 
-#@app.route('/',methods=["GET","PUT","POST"])
-@app.route('/')
+@app.route('/',methods=["GET","PUT","POST"])
+#@app.route('/')
+
 def index():
     #data = json.loads(request.data)
+    #data = request.data # if send in a json string, use request.data.
+    comment = request.form['comment'] # if send in from a html form, use request.form
+    usrname = request.form['usrname']
+    senti = sentiment(comment)[0]
     #msg = data.get('message')
-    msg = 'test'
-    return msg
+    #msg = 'test'
+    return render_template('web_interface.html', comment=comment.strip(), usrname=usrname.strip(),sentiment=senti)
 
 
 if __name__ == '__main__':
